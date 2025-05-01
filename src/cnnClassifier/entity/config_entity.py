51b082eebdd4a82b768adfd6c87bac68c0145977
GOURAV <gouravkcc1@gmail.com>
2025-05-01 17:29:38 +0530

    commit 1

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -13,7 +13,7 @@ class PrepareBaseModelConfig:
     root_dir: Path
     base_model_path: Path
     updated_base_model_path:Path
-    params_image_size: list
+    params_image_size: list #recheck
     params_learning_rate: float
     params_include_top: bool
     params_weights: str
